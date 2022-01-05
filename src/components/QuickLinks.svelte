912c7fc70cbee4a02cf88db2a1bc8779769bac6c
kyle01 <kylemcv@gmail.com>
2022-01-05 10:24:16 -0500

    Create quick link component  and home page grid

---
+++ src/components/QuickLinks.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	const elements = new Set();
+</script>
+
+<div class='border rounded-lg m-4'>
+    <p class='text-center font-bold underline py-4'>Quick links</p>
+    <ul class='m-4 pl-4 underline text-blue-600 hover:text-blue-800 list-disc'>
+        <li class='py-2'>
+            <a href='https://news.ycombinator.com/' target='_blank'>
+                Hacker News
+            </a>
+        </li>
+        <li class='py-2'>
+            <a href='https://darksky.net/' target='_blank'>
+                Dark Sky
+            </a>
+        </li>
+        <li class='py-2'>
+            <a href='https://en.wikipedia.org/wiki/Main_Page' target='_blank'>
+                Wikipedia Home
+            </a>
+        </li>
+    </ul>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p class='text-emerald-600'>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import QuickLinks from '../components/QuickLinks.svelte'
+    const time = new Date();
+</script>
+
+<div class='w-screen h-screen bg-blue-200'>
+    <p class='py-4 text-4xl text-center'>Welcome</p>
+    <p class='py-8 text-2xl text-center'>{time.getHours()}:{time.getMinutes()}</p>
+    <div class='grid grid-cols-3'>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Weather</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Search</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Music</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Stocks</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>US Covid numbers</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Crypto</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Bloomberg Headlines</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>Wikipedia headlines</p>
+        </div>
+        <div class='h-12 border rounded-lg m-4'>
+            <p class='text-center font-bold'>NYTimes Headlines</p>
+        </div>
+        <QuickLinks />
+    </div>
+    
+</div>
