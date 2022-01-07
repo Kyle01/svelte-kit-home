45a2d142ddac7d26af33e7a5fe9b17b7afa8fe2b
kyle01 <kylemcv@gmail.com>
2022-01-07 14:18:12 -0500

    Get accurate weather forecast powered by external api

--- src/components/Weather.svelte
+++ src/components/Weather.svelte
@@ -4,12 +4,19 @@
 
 <script lang='ts'>
     import { onMount } from 'svelte';
-    let temperature;
+    import { format, fromUnixTime } from 'date-fns';
+    let weather;
+    let sunriseDisplay;
+    let sunsetDisplay;
 
     onMount(async () => {
         fetch('/api/weather')
             .then(response => response.json())
-            .then(data => temperature = data.temperature);
+            .then(data => {
+                weather = data;
+                sunriseDisplay = format(fromUnixTime(data.sunrise), 'h:mm a');
+                sunsetDisplay = format(fromUnixTime(data.sunset), 'h:mm a')
+            });
     })
 
 </script>
@@ -17,8 +24,8 @@
 
 <div class='border-4 rounded-lg m-4'>
     <p class='text-center font-bold'>Weather</p>
-    <p>Forcast: </p>
-    <p>Temperature: {temperature ? temperature : 'loading…'}</p>
-    <p>Sunrise: </p>
-    <p>Sunset: </p>
+    <p>Forecast: {weather ? weather.forecast : 'loading…'}</p>
+    <p>Temperature: {weather ? weather.temperature : 'sloading…'}</p>
+    <p>Sunrise: {sunriseDisplay ? sunriseDisplay : 'loading…'}</p>
+    <p>Sunset: {sunsetDisplay ? sunsetDisplay  : 'loading…'}</p>
 </div>
